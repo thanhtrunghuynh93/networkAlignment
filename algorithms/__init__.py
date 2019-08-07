778ec1f17ad23e522e65070c086fe394ff5c3049
vinhsuhi <vinhbachkhoait@gmail.com>
2019-08-08 03:50:06 +0700

    fix init

---
+++ algorithms/__init__.py
@@ -0,0 +1,10 @@
+from algorithms.BigAlign import BigAlign
+from algorithms.DeepLink import DeepLink
+from algorithms.FINAL import FINAL
+from algorithms.IONE import IONE
+from algorithms.IsoRank import IsoRank
+from algorithms.REGAL import REGAL
+from algorithms.PALE import PALE
+
+__all__ = ['BigAlign', 'DeepLink', 'FINAL', 'IONE', 'IsoRank', 'NAWAL', 'REGAL', 'FAGCN', 'PALE']
+
